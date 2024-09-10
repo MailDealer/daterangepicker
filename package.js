1f2d8123d7c4118cdc473f1f939b0260bdce7184
Alexey Yanchenko <your.elkin@gmail.com>
2024-09-10 10:00:37 +0700

    Remove dependencies

--- package.js
+++ package.js
@@ -9,9 +9,6 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0.1');
 
-  api.use('momentjs:moment@2.22.1', ["client"]);
-  api.use('jquery@3.3.1', ["client"]);
-
   api.addFiles('daterangepicker.js', ["client"]);
   api.addFiles('daterangepicker.css', ["client"]);
 });
